90f541d2332dd59563e3bb4b39b3d4f6f791d073
Venom <42916774+Ve6om@users.noreply.github.com>
2026-06-11 08:42:17 -0400

    Add NeoForge maven to root plugin repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://repo.spongepowered.org/repository/maven-public/")
         maven("https://maven.fabricmc.net/")
+        maven("https://maven.neoforged.net/releases")
         maven("https://maven.architectury.dev/")
         maven("https://maven.minecraftforge.net/")
     }
